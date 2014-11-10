7ed857cbc6224676c995fdff65d168a1bad21d4d
Michael Vezzani <mjvezzani@gmail.com>
2014-11-10 10:22:11 -0500

    Create Book, Author, and Review migrations and models

--- db/migrate/20141110151347_create_books.rb
+++ db/migrate/20141110151347_create_books.rb
@@ -1,6 +1,10 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
+      t.string :title
+      t.integer :author_id, :user_id
+
+      t.timestamps
     end
   end
 end
