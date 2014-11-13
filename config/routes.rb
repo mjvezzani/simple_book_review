12929bc46dacff7caf0b2f22cae701eddbf3da5b
Michael Vezzani <mjvezzani@gmail.com>
2014-11-13 06:16:34 -0500

    Browse by author

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'books#index'
   get 'books/search' => 'books#search'
   resources :books
+  resources :authors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 authors = Author.create([{first_name: 'Author', last_name: 'One'}, {first_name: 'Author', last_name: 'Two'}, {first_name: 'Author', last_name: 'Three'}])
-books = Book.create([{title: 'Book 1', author: authors[0]}, {title: 'Book 2', author: authors[1]}, {title: 'Book 3', author: authors[2]}])
+books = Book.create([{title: 'Book 1', author: authors[0]}, {title: 'Book 2', author: authors[1]}, {title: 'Book 3', author: authors[2]}, {title: 'Book 4', author: authors[0]}, {title: 'Book 5', author: authors[1]}, {title: 'Book 6', author: authors[2]}])
